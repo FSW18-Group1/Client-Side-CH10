90e97a3f8d2c75940fd10f416b5119d0c277b328
Yoganarayana <68230425+narayoga@users.noreply.github.com>
2022-06-11 21:56:20 +0800

    fixed component

--- component/dropMenu.js
+++ component/dropMenu.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 
-export default function DropMenu() {
+export default function DropMenu(props) {
     const {user} = props
+    const router = useRouter()
     const logout = () => {
-        removeCookie('token')
-        removeCookie('data')
-        setAuthenticated(false)
+        localStorage.clear()
         router.push('/login')
     }
 
@@ -13,6 +13,11 @@ export default function DropMenu() {
         <div className="dropdown">
             <button className="profile"></button>
             <div className="dropdown-content">
+                <div>
+                    <p className='text-black text-center no-space'>{user.username}</p>
+                    <p className='text-black text-center no-space mb-2'>{user.email}</p>
+                </div>
+                <div className='bottom-line'></div>
                 <Link href={'/profile'}><a>setting</a></Link>
                 <Link href={'#'}><a onClick={logout}>Logout</a></Link>
             </div>
