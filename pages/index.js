e15a13be7574905ec889dc61bc27e70764c0df4e
Nicholas Adi <nicholasadi22@gmail.com>
2022-06-15 01:28:52 +0700

    finishing

--- pages/index.js
+++ pages/index.js
@@ -16,43 +16,32 @@ export default function Home() {
   const { token, data } = useSelector((state) => state.authenticatedReducer);
   const router = useRouter;
 
-    useEffect(()=> {
-        console.log('1. masuk useEffect')
-        document.title = 'Home'
-        dispatch(getLeaderboard())
-        dispatch(authenticatedAction())
-    }, [dispatch])
+  useEffect(()=> {
+      console.log('1. masuk useEffect')
+      document.title = 'Home'
+      dispatch(getLeaderboard())
+      dispatch(authenticatedAction())
+  }, [dispatch])
 
-    return(
-        <>        
-        <div className="section banner-bg">
-            {token && 
-            <div className='d-flex justify-content-between pt-4 mx-3 sticky'>
-                <div className='d-flex justify-content-between nav-side' style={{paddingLeft:"5em"}}>
-                    <Link href='/'><a className='ahref'>Letitgo-game</a></Link>
-                    <p >| home</p>
-                </div>
-                <div className='search'>
-                    <SearchBar />
-                </div>
-                <div className='d-flex justify-content-between nav-side' style={{paddingRight:"2em"}}>
-                    <DropMenu user = {data} />
-                </div>
+  return(
+    <>
+      <div className="section banner-bg">
+        {token && 
+          <div className='d-flex justify-content-between pt-4 mx-3 sticky'>
+            <div className='d-flex justify-content-between nav-side' style={{paddingLeft:"5em"}}>
+                <Link href='/'><a className='ahref'>Letitgo-game</a></Link>
+                <p >| home</p>
             </div>
-            <div className="search">
-              <SearchBar />
+            <div className='search'>
+                <SearchBar />
             </div>
-            <div
-              className="d-flex justify-content-between nav-side"
-              style={{ paddingRight: "2em" }}
-            >
-              <DropMenu user={data} />
+            <div className='d-flex justify-content-between nav-side' style={{paddingRight:"2em"}}>
+                <DropMenu user = {data} />
             </div>
           </div>
-        )}
+        }
 
         {!token && <Navbar />}
-
         <div className="banner">
           <h1 style={{ fontSize: "64px", letterSpacing: "3px" }}>
             Play Free HD Games <br /> And Many Videos
