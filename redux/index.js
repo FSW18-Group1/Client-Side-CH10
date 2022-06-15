3b3e3d0b6654030cbf03be4245e7b0038e339671
Yoganarayana <68230425+narayoga@users.noreply.github.com>
2022-06-15 21:14:20 +0800

    Add files via upload

--- redux/index.js
+++ redux/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from "redux";
 import {signupReducer, loginReducer} from "./reducers/register";
 import authenticatedReducer from "./reducers/authenticated";
 import leaderboardReducer from "./reducers/leaderboard";
-import ProfilePlayerReducer from "./reducers/profileplayer"
 
 export default combineReducers({
     signupReducer,
     loginReducer,
     authenticatedReducer,
-    leaderboardReducer,
-    ProfilePlayerReducer,
+    leaderboardReducer
 })
